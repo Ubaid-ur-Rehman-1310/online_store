efa27bca954168d2b1d6a81a16e24f5ada949bce
Ubaid-ur-Rehman <ubaidurrehman.it@gmail.com>
2023-11-30 08:51:57 -0800

    update database table

--- database_table.py
+++ database_table.py
@@ -4,70 +4,70 @@ import mysql.connector
 db = mysql.connector.connect(
     host="localhost",
     user="root",
-    passwd="ubaid1999",
+    password="ubaid1999",
     database="online_store"
 )
 
 cursor = db.cursor()
 
 # create tables
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS Products (
-    product_id INT AUTO_INCREMENT PRIMARY KEY,
-    product_name VARCHAR(255) NOT NULL,
-    product_price DECIMAL(10,2) NOT NULL
-)
-""")
 
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS Users (
+create_user_table = """
+CREATE TABLE IF NOT EXISTS user (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
-    user_name VARCHAR(255) NOT NULL,
-    user_password VARCHAR(225) NOT NULL,
-    user_email VARCHAR(255) NOT NULL
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
 )
-""")
+"""
 
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS Cart (
-    cart_id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES User(user_id)
+create_product_table = """
+CREATE TABLE IF NOT EXISTS product (
+    product_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL
 )
-""")
+"""
 
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS Cart_items (
+create_cart_item_table = """
+CREATE TABLE IF NOT EXISTS cart_item (
     cart_item_id INT AUTO_INCREMENT PRIMARY KEY,
-    cart_id INT NOT NULL,
-    product_id INT NOT NULL,
-    quantity INT NOT NULL,
-    FOREIGN KEY (cart_id) REFERENCES Cart(cart_id),
-    FOREIGN KEY (product_id) REFERENCES Products(product_id)
+    user_id INT,
+    product_id INT,
+    quantity INT,
+    FOREIGN KEY (user_id) REFERENCES user(user_id),
+    FOREIGN KEY (product_id) REFERENCES product(product_id)
 )
-""")
+"""
 
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS Orders (
+create_cart_table = """
+CREATE TABLE IF NOT EXISTS cart (
+    cart_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    FOREIGN KEY (user_id) REFERENCES user(user_id)
+)
+"""
+
+create_orders_table = """
+CREATE TABLE IF NOT EXISTS orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT NOT NULL,
-    order_date DATE NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES User(user_id)
+    user_id INT,
+    product_id INT,
+    quantity INT,
+    total_amount DECIMAL(10, 2),
+    order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES user(user_id)
 )
-""")
-
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS Order_items (
-    order_item_id INT AUTO_INCREMENT PRIMARY KEY,
-    order_id INT NOT NULL,
-    product_id INT NOT NULL,
-    quantity INT NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
-    FOREIGN KEY (product_id) REFERENCES Products(product_id)
-)
-""")
-
- #insert sample data for products
+"""
+# # Execute table creation queries
+cursor.execute(create_user_table)
+cursor.execute(create_product_table)
+cursor.execute(create_cart_item_table)
+cursor.execute(create_cart_table)
+cursor.execute(create_orders_table)
+# # Committing the changes
+db.commit()
+# insert sample data for products
 data_to_insert = [
     ('Laptop', 999.99),
     ('Smartphone', 499.99),
@@ -90,9 +90,6 @@ data_to_insert = [
     ('Activity Tracker', 49.99),
     ('Wireless Earbuds', 129.99)
  ]
-
-cursor.executemany("INSERT INTO Products (product_name, product_price) VALUES (%s, %s)", data_to_insert)
+cursor.executemany("INSERT INTO product (name, price) VALUES (%s, %s)", data_to_insert)
 print('Inserted products into the database')
 db.commit()
-
-
